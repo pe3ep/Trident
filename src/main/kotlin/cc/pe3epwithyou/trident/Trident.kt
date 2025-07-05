543e1d316f40a6fb8bdd095711e6d94b72f5d7c9
Andrew <74493360+pe3ep@users.noreply.github.com>
2025-07-05 14:37:19 +0300

    Initial commit

---
+++ src/main/kotlin/cc/pe3epwithyou/trident/Trident.kt
@@ -0,0 +1,9 @@
+package cc.pe3epwithyou.trident
+
+import net.fabricmc.api.ModInitializer
+
+class Trident : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/cc/pe3epwithyou/trident/client/TridentClient.kt
@@ -0,0 +1,9 @@
+package cc.pe3epwithyou.trident.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class TridentClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
