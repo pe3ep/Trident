08465c0fe272e49fb6a2835f976964dbfa9e2c9e
Andrew <74493360+pe3ep@users.noreply.github.com>
2025-08-13 01:07:42 +0300

    Added quest completion criteria

--- src/main/kotlin/cc/pe3epwithyou/trident/widgets/questing/Quest.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/widgets/questing/Quest.kt
@@ -2,38 +2,130 @@ package cc.pe3epwithyou.trident.widgets.questing
 
 import cc.pe3epwithyou.trident.state.MCCGame
 import cc.pe3epwithyou.trident.state.Rarity
+import cc.pe3epwithyou.trident.widgets.questing.CompletionCriteria.*
+import net.minecraft.resources.ResourceLocation
 
 class Quest(
     val game: MCCGame,
     val type: QuestType,
     val rarity: Rarity,
-    val completionRequirement: QuestCompletionRequirement
-)
-
-
-class QuestCompletionRequirement(
     val criteria: CompletionCriteria,
     val initialProgress: Int,
     val totalProgress: Int,
-    private val nameArg: String? = null
 ) {
-    val name: String
+    val sprite: ResourceLocation
         get() {
-            val n = criteria.shortName
-            if (nameArg != null) {
-                n.replace("{value}", nameArg)
-            }
-            return n
+            val directory = type.directoryPath
+            val raritySuffix = rarity.name.lowercase()
+            val sprite = ResourceLocation.fromNamespaceAndPath("mcc", directory + raritySuffix)
+            return sprite
         }
+
+    val display_name: String = criteria.shortName
 }
 
-enum class QuestType {
-    DEFAULT,
-    SCROLL,
-    BOOSTED,
-    ARCANE,
+enum class QuestType(
+    val directoryPath: String
+) {
+    DEFAULT(
+        "island_interface/quest_log/daily/"
+    ),
+    SCROLL(
+        "island_interface/quest_log/scrolls/"
+    ),
+    BOOSTED(
+        "island_interface/quest_log/boosted/"
+    ),
+    ARCANE(
+        "island_interface/quest_log/arcane/"
+    );
 }
 
-enum class CompletionCriteria(val shortName: String) {
-
+enum class GameQuests(
+    val list: List<CompletionCriteria>
+) {
+    HITW(
+        listOf(
+            HOLE_IN_THE_WALL_SURVIVED_TWO_MINUTE,
+            HOLE_IN_THE_WALL_SURVIVED_MINUTE,
+            HOLE_IN_THE_WALL_WALLS_DODGED,
+            HOLE_IN_THE_WALL_TOP_EIGHT,
+            HOLE_IN_THE_WALL_TOP_FIVE,
+            HOLE_IN_THE_WALL_TOP_THREE
+        )
+    ),
+    TGTTOS(
+        listOf(
+            TGTTOS_CHICKENS_PUNCHED,
+            TGTTOS_TOP_EIGHT,
+            TGTTOS_TOP_FIVE,
+            TGTTOS_TOP_THREE,
+            TGTTOS_ROUND_TOP_EIGHT,
+            TGTTOS_ROUND_TOP_FIVE,
+            TGTTOS_ROUND_TOP_THREE
+        )
+    ),
+    BATTLE_BOX(
+        listOf(
+            BATTLE_BOX_QUADS_GAMES_PLAYED,
+            BATTLE_BOX_QUADS_TEAM_ROUNDS_WON,
+            BATTLE_BOX_QUADS_TEAM_FIRST_PLACE,
+            BATTLE_BOX_QUADS_TEAM_SECOND_PLACE,
+            BATTLE_BOX_QUADS_PLAYERS_KILLED,
+            BATTLE_BOX_QUADS_RANGED_KILLS
+        )
+    ),
+    SKY_BATTLE(
+        listOf(
+            SKY_BATTLE_QUADS_SURVIVED_TWO_MINUTE,
+            SKY_BATTLE_QUADS_SURVIVED_MINUTE,
+            SKY_BATTLE_QUADS_SURVIVAL_TOP_TEN,
+            SKY_BATTLE_QUADS_SURVIVAL_TOP_FIVE,
+            SKY_BATTLE_QUADS_SURVIVAL_TOP_THREE,
+            SKY_BATTLE_QUADS_PLAYERS_KILLED
+        )
+    ),
+    PARKOUR_WARRIOR_SURVIVOR(
+        listOf(
+            PW_SURVIVAL_OBSTACLES_COMPLETED,
+            PW_SURVIVAL_PLAYERS_ELIMINATED,
+            PW_SURVIVAL_LEAP_6_COMPLETION,
+            PW_SURVIVAL_LEAP_4_COMPLETION,
+            PW_SURVIVAL_LEAP_2_COMPLETION
+        )
+    ),
+    PARKOUR_WARRIOR_DOJO(
+        listOf(
+            PW_SOLO_TOTAL_MEDALS_BANKED,
+            PW_SOLO_STANDARD_CMPL_BELOW_FIVE_MIN,
+            PW_SOLO_STANDARD_CMPL_BELOW_THREE_MIN,
+            PW_SOLO_STANDARD_CMPL_BELOW_TWO_MIN,
+            PW_SOLO_ADVANCED_CMPL_BELOW_FIVE_MIN,
+            PW_SOLO_ADVANCED_CMPL_BELOW_FOUR_MIN,
+            PW_SOLO_TOTAL_ADVANCED_CMPLS,
+            PW_SOLO_TOTAL_STANDARD_CMPLS
+        )
+    ),
+    DYNABALL(
+        listOf(
+            DYNABALL_SURVIVE_1M,
+            DYNABALL_SURVIVE_2M,
+            DYNABALL_SURVIVE_4M,
+            DYNABALL_WINS,
+            DYNABALL_PLAYERS_ELIMINATED,
+            DYNABALL_PLAYERS_STUCK,
+            DYNABALL_BLOCKS_DESTROYED,
+            DYNABALL_BLOCKS_PLACED
+        )
+    ),
+    ROCKET_SPLEEF_RUSH(
+        listOf(
+            ROCKET_SPLEEF_PLAYERS_OUTLIVED,
+            ROCKET_SPLEEF_TOP_FIVE,
+            ROCKET_SPLEEF_TOP_EIGHT,
+            ROCKET_SPLEEF_TOP_THREE,
+            ROCKET_SPLEEF_SURVIVE_60S,
+            ROCKET_SPLEEF_DIRECT_HITS
+        )
+    )
 }
