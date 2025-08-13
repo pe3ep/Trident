4e9f5f0b31644acd878e75ebe827439a29ae1a35
Andrew <74493360+pe3ep@users.noreply.github.com>
2025-08-14 02:57:07 +0300

    Improve performance when rendering rounded backgrounds

--- src/main/kotlin/cc/pe3epwithyou/trident/dialogs/TridentDialog.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/dialogs/TridentDialog.kt
@@ -1,43 +1,20 @@
 package cc.pe3epwithyou.trident.dialogs
 
 import cc.pe3epwithyou.trident.utils.ChatUtils
+import cc.pe3epwithyou.trident.utils.GraphicsExtensions.fillRoundedAll
 import com.noxcrew.sheeplib.dialog.Dialog
 import net.minecraft.client.gui.GuiGraphics
-import net.minecraft.client.renderer.RenderPipelines
-import net.minecraft.client.renderer.RenderType
-import net.minecraft.resources.ResourceLocation
-import kotlin.jvm.optionals.getOrNull
 
 abstract class TridentDialog(x: Int, y: Int, private val key: String) : Dialog(x, y) {
-    private companion object {
-        private val ROUNDED_BOTTOM = ResourceLocation.fromNamespaceAndPath("trident", "interface/background/rounded_bottom")
-        private val ROUNDED_ALL = ResourceLocation.fromNamespaceAndPath("trident", "interface/background/rounded_all")
-        private val BORDER_BOTTOM = ResourceLocation.fromNamespaceAndPath("trident", "interface/background/border/border_bottom")
-        private val BORDER_ALL = ResourceLocation.fromNamespaceAndPath("trident", "interface/background/border/border_all")
-    }
-
     override fun renderBackground(graphics: GuiGraphics) {
-        graphics.blitSprite(
-            RenderPipelines.GUI_TEXTURED,
-            ROUNDED_ALL,
+        val color = if (parent == null) theme.colors.dialogBackgroundAlt else theme.colors.dialogBackground
+        graphics.fillRoundedAll(
             x,
             y,
             getWidth(),
             getHeight(),
-            if (parent == null) theme.colors.dialogBackgroundAlt else theme.colors.dialogBackground
+            color
         )
-
-        if (theme.dialogBorders) {
-            graphics.blitSprite(
-                RenderPipelines.GUI_TEXTURED,
-                BORDER_ALL,
-                x,
-                y,
-                getWidth(),
-                getHeight(),
-                theme.colors.border
-            )
-        }
     }
 
     open fun refresh() {
