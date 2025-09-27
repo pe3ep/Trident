80b23326e90837a8fea4f1d8a921498c821dd1ec
Andrew <legendarysuwe@gmail.com>
2025-09-27 13:36:07 +0300

    :bug: Fix 'This spot is depleted' title showing after spot reset

--- src/main/kotlin/cc/pe3epwithyou/trident/feature/fishing/DepletedDisplay.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/feature/fishing/DepletedDisplay.kt
@@ -5,6 +5,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.client.Minecraft
 import net.minecraft.network.chat.Component
 import net.minecraft.world.phys.Vec3
+import java.time.Instant
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
 
 object DepletedDisplay {
     private const val DEPLETED_COLOR = 0xf27500
@@ -27,19 +30,25 @@ object DepletedDisplay {
         private var ticks: Long = 0
         private var playerPosition: Vec3 = Vec3(0.0, 0.0, 0.0)
         private var title: Component = depletedTitle
+        private var castAt: Instant? = null
         fun startLoop(component: Component, waitFor: Long) {
             this.playerPosition = Minecraft.getInstance().player?.position()!!
             this.ticks = waitFor
             this.title = component
+            this.castAt = Instant.now()
         }
 
         fun stopLoop() {
             playerPosition = Vec3(0.0, 0.0, 0.0)
             this.ticks = 0
+            this.castAt = null
         }
 
         override fun onEndTick(client: Minecraft) {
             if (--this.ticks == 0L) {
+                if (castAt != null && hasHourPassed(castAt!!)) {
+                    stopLoop()
+                }
                 Title.sendTitle(
                     Component.empty(),
                     this.title,
@@ -59,6 +68,15 @@ object DepletedDisplay {
             fun register() {
                 ClientTickEvents.END_CLIENT_TICK.register(INSTANCE)
             }
+        }
+
+        fun hasHourPassed(saved: Instant, zone: ZoneId = ZoneId.systemDefault()): Boolean {
+            val now = Instant.now().atZone(zone).toInstant()
+            val savedHourStart = saved.atZone(zone)
+                .truncatedTo(ChronoUnit.HOURS)
+                .toInstant()
+            val nextHourStart = savedHourStart.plus(1, ChronoUnit.HOURS)
+            return !now.isBefore(nextHourStart)
         }
     }
 }
