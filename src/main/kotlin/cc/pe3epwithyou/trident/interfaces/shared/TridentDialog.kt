2a90a4632c161bb1bcafe55d9b0ed6ee586d2a5e
Andrew <legendarysuwe@gmail.com>
2025-10-03 22:15:03 +0300

    :construction: WIP Layout portal improvements

--- src/main/kotlin/cc/pe3epwithyou/trident/interfaces/shared/TridentDialog.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/interfaces/shared/TridentDialog.kt
@@ -19,7 +19,11 @@ abstract class TridentDialog(x: Int, y: Int, private val key: String) : Dialog(x
         )
     }
 
-    fun publicLayout() = layout()
+    /**
+     * Used to access dialog's layout
+     */
+    val publicLayout
+        get() = layout()
 
     open fun refresh() {
         val screenWidth = Minecraft.getInstance().window.guiScaledWidth
