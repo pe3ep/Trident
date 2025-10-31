676750c1671872ab88e1c7bd4c8918b6c6fe0dcd
Andrew <legendarysuwe@gmail.com>
2025-10-31 20:34:47 +0300

    :construction: Attachable and Detachable tabs

--- src/main/kotlin/cc/pe3epwithyou/trident/interfaces/experiment/widgets/Tab.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/interfaces/experiment/widgets/Tab.kt
@@ -1,5 +1,6 @@
 package cc.pe3epwithyou.trident.interfaces.experiment.widgets
 
+import cc.pe3epwithyou.trident.utils.Resources
 import cc.pe3epwithyou.trident.utils.Texture
 import net.minecraft.client.gui.layouts.Layout
 import net.minecraft.network.chat.Component
@@ -10,4 +11,14 @@ data class Tab(
     val title: Component,
     var isDetached: Boolean = false,
     val layout: () -> Layout
-)
+) {
+    companion object {
+        val DETACH_ICON: Texture = Texture(
+            Resources.trident("textures/interface/dialog_actions/detach.png"), 8, 8
+        )
+
+        val ATTACH_ICON: Texture = Texture(
+            Resources.trident("textures/interface/dialog_actions/attach.png"), 8, 8
+        )
+    }
+}
