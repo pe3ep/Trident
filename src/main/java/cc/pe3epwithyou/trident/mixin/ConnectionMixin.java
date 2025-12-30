91ce5d8a2348fd02821af7abbb1b5ab9def04b70
Andrew <legendarysuwe@gmail.com>
2025-12-30 17:15:16 +0300

    :bug: Fix disconnect detection not working as expected

--- src/main/java/cc/pe3epwithyou/trident/mixin/ConnectionMixin.java
+++ src/main/java/cc/pe3epwithyou/trident/mixin/ConnectionMixin.java
@@ -4,20 +4,42 @@ import cc.pe3epwithyou.trident.interfaces.DialogCollection;
 import cc.pe3epwithyou.trident.state.PlayerStateIO;
 import cc.pe3epwithyou.trident.utils.ChatUtils;
 import cc.pe3epwithyou.trident.utils.DelayedAction;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
+import net.minecraft.network.PacketListener;
 import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 @Mixin(Connection.class)
-public class ConnectionMixin {
-    @Inject(method = "disconnect(Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"))
+public abstract class ConnectionMixin {
+    @Shadow
+    public abstract SocketAddress getRemoteAddress();
+
+    @Shadow
+    private volatile @Nullable PacketListener packetListener;
+
+    @Inject(method = "disconnect(Lnet/minecraft/network/chat/Component;)V", at = @At("TAIL"))
     private void onDisconnect(Component component, CallbackInfo ci) {
-        ChatUtils.INSTANCE.info("Disconnected from a server, cancelling all pending tasks");
-        DelayedAction.INSTANCE.closeAllPendingTasks();
-        PlayerStateIO.INSTANCE.save();
-        DialogCollection.INSTANCE.saveAllDialogs();
+        SocketAddress remoteAddress = getRemoteAddress();
+        if (remoteAddress == null) return;
+        if (remoteAddress instanceof InetSocketAddress inetSocketAddress) {
+            String ip = inetSocketAddress.getAddress().getHostName();
+            if (ip == null) return;
+            Minecraft minecraft = Minecraft.getInstance();
+            if (ip.contains("mccisland.net") && packetListener == minecraft.getConnection()) {
+                DelayedAction.INSTANCE.closeAllPendingTasks();
+                DialogCollection.INSTANCE.saveAllDialogs();
+                PlayerStateIO.INSTANCE.save();
+                ChatUtils.INSTANCE.info("Disconnected from MCC Island at IP: " + ip);
+            }
+        }
     }
 }
