8f61d1c6d8b9fa591cd3f667c2bac11853dec5ea
Andrew <legendarysuwe@gmail.com>
2026-01-26 02:27:31 +0300

    :lipstick: Small UI improvements to reply lock widget

--- src/main/kotlin/cc/pe3epwithyou/trident/feature/dmlock/CurrentLockedChatWidget.kt
+++ src/main/kotlin/cc/pe3epwithyou/trident/feature/dmlock/CurrentLockedChatWidget.kt
@@ -1,8 +1,11 @@
 package cc.pe3epwithyou.trident.feature.dmlock
 
+import cc.pe3epwithyou.trident.config.Config
 import cc.pe3epwithyou.trident.feature.dmlock.ReplyLock.currentLock
+import cc.pe3epwithyou.trident.utils.Resources
 import cc.pe3epwithyou.trident.utils.extensions.ComponentExtensions.mccFont
 import cc.pe3epwithyou.trident.utils.extensions.ComponentExtensions.popped
+import cc.pe3epwithyou.trident.utils.extensions.ComponentExtensions.withFont
 import cc.pe3epwithyou.trident.utils.extensions.ComponentExtensions.withTridentFont
 import cc.pe3epwithyou.trident.utils.extensions.GraphicsExtensions.fillRoundedAll
 import com.noxcrew.sheeplib.util.opacity
@@ -23,14 +26,18 @@ class CurrentLockedChatWidget(val user: String) : AbstractWidget(0, 0, 0, 9, Com
 
     init {
         val lock = Component.literal("\uE016").withTridentFont().popped()
-        displayedComponent = lock.append(
-            Component.literal(" REPLY LOCK: ").mccFont().withColor(0xFFFFFF)
+        displayedComponent = lock
+            .append(
+                Component.literal("\uE002").withFont(Resources.minecraft("padding"))
+            )
+            .append(
+            Component.literal("REPLY LOCK: ").mccFont().withColor(0xFFFFFF)
                 .append(Component.literal(user.uppercase()).withColor(0xfc7dfc))
         ).withoutShadow()
         width =
             Minecraft.getInstance().font.width(
                 displayedComponent
-            ) + PADDING * 2 + 4
+            ) + PADDING * 2 + 3
     }
 
     override fun renderWidget(
@@ -40,9 +47,9 @@ class CurrentLockedChatWidget(val user: String) : AbstractWidget(0, 0, 0, 9, Com
         f: Float
     ) {
         graphics.fillRoundedAll(
-            x, y, width - 4, height,
+            x, y, width - 3, height,
             when {
-                isHovered -> 0x7d1b8c opacity 128
+                isHovered -> 0xa81bb7 opacity 128
                 else -> 0x521d59 opacity 128
             }
         )
@@ -53,6 +60,8 @@ class CurrentLockedChatWidget(val user: String) : AbstractWidget(0, 0, 0, 9, Com
             y,
             0xFFFFFF.opaqueColor()
         )
+        if (!Config.Global.chatChannelButtons) return
+        graphics.fill(x + width - 1, y + height / 2 - 2, x + width, y + height / 2 + 3, 0x000000 opacity 64)
     }
 
     override fun onClick(mouseButtonEvent: MouseButtonEvent, bl: Boolean) {
